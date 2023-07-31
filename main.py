92feb579db93665930587ea1fc8e0bf037672e60
joen1510 <jonapapi86@gmail.com>
2023-07-30 19:58:43 -0500

    segundo commit

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from selenium import webdriver
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from selenium.webdriver.common.by import By
-from time import sleep
+from selenium import webdriver
+from datetime import datetime
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+user = os.getenv('MONGO_USER')
+password = os.getenv('MONGO_PASSWORD')
+hostname = os.getenv('MONGO_HOSTNAME')
+
+uri = f"mongodb+srv://{user}:{password}@{hostname}/?retryWrites=true&w=majority"
+
+mongo_client = MongoClient(uri, server_api=ServerApi('1'))
+
+
+consulta = "chevrolet"
+
 driver = webdriver.Chrome()
 driver.get("https://ecuador.patiotuerca.com/")
 search_box = driver.find_element(by=By.CSS_SELECTOR, value="#search-list")
-search_box.send_keys("Audi A4")
+
+search_box.send_keys(consulta)
 
 search_button = driver.find_element(by=By.CSS_SELECTOR, value="#openSearch > div > div > div.search-ots.false > img")
 search_button.click()
 
 vehicle_cards = driver.find_elements(By.CSS_SELECTOR, "#featuredUsed > div.xl3")
+
 
 for card in vehicle_cards:
     try:
@@ -19,15 +38,25 @@ for card in vehicle_cards:
         print(title)
         print(kms_y_city)
         print(f"${price}")
-        record = {
-            "title": title,
-            "kms_y_city": kms_y_city,
-            "price": price
+
+        coche_actual = {
+            "consulta": consulta,
+            "resultado": {
+                "title": title,
+                "kms_y_city": kms_y_city,
+                "price": price,
+            }
         }
+
+        db = mongo_client.get_database('db_eig')
+        collection = db.get_collection(f'chevrolet')
+
+        collection.insert_one(coche_actual)
+
         print("++++++++++++++++++++++++++++++++")
     except Exception as e:
         print(e)
         print("++++++++++++++++++++++++++++++++")
 
-#sleep(15)
+
 driver.close()
